acbca5a6047f8a21ef81c4d2bd1c8ebf3fa2285c
BopapeTSB <sebatabopape@gmail.com>
2026-05-16 17:11:51 +0200

    Update embeddings.py

--- rag/embeddings.py
+++ rag/embeddings.py
@@ -1,9 +1,17 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer(
-"all-MiniLM-L6-v2"
-)
+_model = None
+
+def get_model():
+    global _model
+    if _model is None:
+        from sentence_transformers import SentenceTransformer
+        _model = SentenceTransformer("all-MiniLM-L6-v2")
+    return _model
+
 
 def create_embedding(text):
+    model = get_model()
     return model.encode(text).tolist()
+
 
